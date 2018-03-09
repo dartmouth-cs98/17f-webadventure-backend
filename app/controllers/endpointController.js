1c2d064d70be1f294fb69e766e666c75c77342e1
Alma Wang <almawang96@gmail.com>
2018-03-08 19:38:29 -0500

    bad paths

--- app/controllers/endpointController.js
+++ app/controllers/endpointController.js
@@ -25,17 +25,18 @@ export const getRandomEndpoint = (callback) => {
         ],
       });
     } else {
-      // endpoint.remove();
-      // callback(cleanEndpoint(endpoint));
-      callback({
-        id: 'end',
-        startPage: 'wikipedia.org/wiki/Mezzaluna',
-        goalPage: 'wikipedia.org/wiki/Pizza',
-        path: [
-          'wikipedia.org/wiki/Mezzaluna',
-          'wikipedia.org/wiki/Pizza',
-        ],
+      endpoint.remove();
+      let pathGood = true;
+      endpoint.path.forEach((url) => {
+        if (url.charAt(0) === '_') {
+          pathGood = false;
+        }
       });
+      if (pathGood) {
+        callback(cleanEndpoint(endpoint));
+      } else {
+        getRandomEndpoint(callback);
+      }
     }
   }).catch((err) => {
     console.log(err);
@@ -46,7 +47,21 @@ export const insertEndpoint = (startPage, goalPage, path = [], callback) => {
   const newEndpoint = new Endpoint();
   newEndpoint.startPage = startPage;
   newEndpoint.goalPage = goalPage;
-  newEndpoint.path = path;
+  const newPath = [];
+  let url = path[0];
+  let i;
+  for (i = 1; i < path.length; i += 1) {
+    if (path[i].charAt(0) === '_') {
+      url += path[i];
+    } else {
+      newPath.push(url);
+      url = path[i];
+    }
+    if (i === path.length - 1) {
+      newPath.push(url);
+    }
+  }
+  newEndpoint.path = newPath;
   newEndpoint.save();
   callback(cleanEndpoint(newEndpoint));
 };
